6eff4d172e771909bdeedf0d60f24cc533e5ffa4
VadimYanovskij <garretnmru@gmail.com>
2020-11-08 19:57:23 +0300

    fix: create ManyToMany relations in User and Role

--- src/main/java/com/senla/training/library/entity/Role.java
+++ src/main/java/com/senla/training/library/entity/Role.java
@@ -1,63 +1,37 @@
 package com.senla.training.library.entity;
 
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
-import java.util.Collection;
+import java.util.Set;
 
 @Entity
-
-@Table(name = "role", schema = "library", catalog = "")
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+@Table(name = "role", schema = "library")
 public class Role {
     private Integer id;
     private String roleName;
-    private Collection<UserRole> userRolesById;
+    private Set<User> users;
 
     @Id
-    @Column(name = "id", nullable = false)
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Integer getId() {
         return id;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
     @Basic
-    @Column(name = "role_name", nullable = false, length = 45)
+    @Column(name = "role_name")
     public String getRoleName() {
         return roleName;
     }
 
-    public void setRoleName(String roleName) {
-        this.roleName = roleName;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Role that = (Role) o;
-
-        if (id != null ? !id.equals(that.id) : that.id != null) return false;
-        if (roleName != null ? !roleName.equals(that.roleName) : that.roleName != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (roleName != null ? roleName.hashCode() : 0);
-        return result;
-    }
-
-    @OneToMany(mappedBy = "roleByRoleId")
-    public Collection<UserRole> getUserRolesById() {
-        return userRolesById;
-    }
-
-    public void setUserRolesById(Collection<UserRole> userRolesById) {
-        this.userRolesById = userRolesById;
+    @ManyToMany(mappedBy = "role", fetch = FetchType.LAZY)
+    public Set<User> getUsers() {
+        return users;
     }
 }
--- src/main/java/com/senla/training/library/entity/User.java
+++ src/main/java/com/senla/training/library/entity/User.java
@@ -1,11 +1,21 @@
 package com.senla.training.library.entity;
 
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.sql.Date;
+import java.time.LocalDate;
 import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 @Entity
-@Table(name = "user", schema = "library", catalog = "")
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+@Table(name = "user", schema = "library")
 public class User {
     private Integer id;
     private String username;
@@ -13,9 +23,8 @@ public class User {
     private String password;
     private String firstname;
     private String lastname;
-    private Date birthday;
-    private Collection<Borrow> borrowsById;
-    private Collection<UserRole> userRolesById;
+    private LocalDate birthday;
+    private Set<Role> roles;
 
     @Id
     @Column(name = "id", nullable = false)
@@ -24,18 +33,10 @@ public class User {
         return id;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
     @Basic
     @Column(name = "username", nullable = false, length = 45)
     public String getUsername() {
         return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 
     @Basic
@@ -44,18 +45,10 @@ public class User {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     @Basic
     @Column(name = "password", nullable = false, length = 256)
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     @Basic
@@ -64,75 +57,24 @@ public class User {
         return firstname;
     }
 
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
     @Basic
     @Column(name = "lastname", nullable = false, length = 45)
     public String getLastname() {
         return lastname;
     }
 
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
     @Basic
     @Column(name = "birthday", nullable = false)
-    public Date getBirthday() {
+    public LocalDate getBirthday() {
         return birthday;
     }
 
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")})
+    public Set<Role> getRoles() {
+        return roles;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        User that = (User) o;
-
-        if (id != null ? !id.equals(that.id) : that.id != null) return false;
-        if (username != null ? !username.equals(that.username) : that.username != null) return false;
-        if (email != null ? !email.equals(that.email) : that.email != null) return false;
-        if (password != null ? !password.equals(that.password) : that.password != null) return false;
-        if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
-        if (lastname != null ? !lastname.equals(that.lastname) : that.lastname != null) return false;
-        if (birthday != null ? !birthday.equals(that.birthday) : that.birthday != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (username != null ? username.hashCode() : 0);
-        result = 31 * result + (email != null ? email.hashCode() : 0);
-        result = 31 * result + (password != null ? password.hashCode() : 0);
-        result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
-        result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
-        result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
-        return result;
-    }
-
-    @OneToMany(mappedBy = "userByUserId")
-    public Collection<Borrow> getBorrowsById() {
-        return borrowsById;
-    }
-
-    public void setBorrowsById(Collection<Borrow> borrowsById) {
-        this.borrowsById = borrowsById;
-    }
-
-    @OneToMany(mappedBy = "userByUserId")
-    public Collection<UserRole> getUserRolesById() {
-        return userRolesById;
-    }
-
-    public void setUserRolesById(Collection<UserRole> userRolesById) {
-        this.userRolesById = userRolesById;
-    }
 }
